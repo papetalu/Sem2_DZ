4985dadff88128ab3740c57421de191e7891ce95
Liuba <pl@mail.ru>
2022-09-20 21:31:12 +0300

    Task 1

--- Z10/Program.cs
+++ Z10/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int n;
+
+while (true)
+{
+   n = Convert.ToInt32(Console.ReadLine());
+   if (n >= 100 && n < 1000 || n >= -999 && n < -100)
+   {
+      break;
+   }
+   Console.WriteLine("Число не трехзначное. Попробуйте снова");
+}
+if (n > 0)
+{
+   int x = n % 100;
+   int y = x / 10;
+   Console.WriteLine("Вторая цифра числа: ");
+   Console.WriteLine(y);
+}
+else
+{
+   int x = n % 100;
+   int y = x / -10;
+   Console.WriteLine("Вторая цифра числа: ");
+   Console.WriteLine(y);
+}
