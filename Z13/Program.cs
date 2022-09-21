3e7f6809e1a63274e6ee63e1a473878fe1e0dc61
Liuba <pl@mail.ru>
2022-09-21 22:02:25 +0300

    Task 2

--- Z13/Program.cs
+++ Z13/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+string a = Convert.ToString(new Random().Next(0, 10000));
+if (a.Length >2)
+{
+Console.Write("Число выбрано - " + a + ", третья цифра - ");
+Console.WriteLine(a[2]);
+}
+else
+{
+   Console.Write("Число выбрано - " + a + ", третьей цифры нет");
+
+}
