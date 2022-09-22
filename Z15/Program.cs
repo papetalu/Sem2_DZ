b8e7e6f5070209fba67d5c819ffd5f96e3aa5102
Liuba <pl@mail.ru>
2022-09-22 21:24:33 +0300

    Task 3

--- Z15/Program.cs
+++ Z15/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int n;
+
+while (true)
+{
+   n = Convert.ToInt32(Console.ReadLine());
+   if (n > 0 && n < 8)
+   {
+      break;
+   }
+   Console.WriteLine("Ошибка. Попробуйте снова");
+}
+if (n == 6 || n == 7)
+{
+   Console.WriteLine("Этот день выходной");
+   
+}
+else
+{
+   Console.WriteLine("Это будний день");
+   }
